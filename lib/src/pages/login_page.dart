9c4d408f55b7d722be793688ad54bfe435ec2a7b
zenTCS <jonathan.zheng@tcs.com.ve>
2021-12-05 13:19:42 -0400

    ...

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -12,23 +12,38 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Flexible(
-                child: Image.asset('assets/chavez2.png',
-                  height: 300.0,
+      child: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: NetworkImage('https://wallpaperaccess.com/full/1269989.jpg'),
+          fit: BoxFit.cover,
+        )),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  child: Image.asset(
+                    'assets/chavez2.png',
+                    height: 300.0,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 15.0,),
-              _userrTextField(),
-              const SizedBox(height: 15.0,),
-              _passwordTextField(),
-              const SizedBox(height: 15.0,),
-              _bottonLogin(),
-            ],
+                const SizedBox(
+                  height: 15.0,
+                ),
+                _userrTextField(),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                _passwordTextField(),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                _bottonLogin(),
+              ],
+            ),
           ),
         ),
       ),
@@ -37,59 +52,54 @@ class _LoginPageState extends State<LoginPage> {
 
   Widget _userrTextField() {
     return StreamBuilder(
-      builder: (BuildContext context, AsyncSnapshot snapshot){
-        return Container(
-          padding: const EdgeInsets.symmetric(horizontal: 40.0),
-          child: const TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              icon: Icon(Icons.email),
-              hintText: 'example@correo.com',
-              labelText: 'Email',
-            ),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: const TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            icon: Icon(Icons.email),
+            hintText: 'example@correo.com',
+            labelText: 'Email',
           ),
-        );
-      }
-    );
+        ),
+      );
+    });
   }
 
   Widget _passwordTextField() {
     return StreamBuilder(
-      builder: (BuildContext context, AsyncSnapshot snapshot){
-        return Container(
-          padding: const EdgeInsets.symmetric(horizontal: 40.0),
-          child: const TextField(
-            keyboardType: TextInputType.emailAddress,
-            obscureText: true,
-            decoration: InputDecoration(
-              icon: Icon(Icons.lock),
-              hintText: 'Password',
-              labelText: 'Password',
-            ),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: const TextField(
+          keyboardType: TextInputType.emailAddress,
+          obscureText: true,
+          decoration: InputDecoration(
+            icon: Icon(Icons.lock),
+            hintText: 'Password',
+            labelText: 'Password',
           ),
-        );
-      }
-    );
+        ),
+      );
+    });
   }
 
   Widget _bottonLogin() {
     return StreamBuilder(
-      builder: (BuildContext context, AsyncSnapshot snapshot){
-        return ElevatedButton(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return ElevatedButton(
           child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-            child: const Text('Sign In',
-              style: TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold
-              ),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+            child: const Text(
+              'Sign In',
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
             ),
           ),
-          onPressed: (){
+          onPressed: () {
             Navigator.pushNamed(context, Routes.SPLASH);
-          }
-        );
-      }
-    );
+          });
+    });
   }
 }
